cfa4dbe4eed980f05ad2ef7d212c33e173368dc6
MaySparkling <Apriantosmk3@gmail.com>
2026-05-29 19:57:58 +0700

    .

--- resources/views/Kelas/index.blade.php
+++ resources/views/Kelas/index.blade.php
@@ -165,6 +165,7 @@
 
                 <thead>
             <tr>
+                <th>id</th>
                 <th>Kode Kelas</th>
                 <th>Nama Dosen</th>
                 <th>Nama Mata Kuliah</th>
@@ -181,11 +182,13 @@
             @foreach($kelas as $c)
 
             <tr>
+                <td>{{ $c->id }}</td>
+
                 <td>{{ $c->kode_kelas }}</td>
 
-                <td>{{ $c->kode_dosen }}</td>
+                <td>{{ $c->dosen->Fullname }}</td>
 
-                <td>{{ $c->kode_mata_kuliah }}</td>
+                <td>{{ $c->mata_kuliah->Nama_Mata_Kuliah}}</td>
 
                 <td>{{ $c->ruang_kelas }}</td>
 
