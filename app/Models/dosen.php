31a4ab17f5d97106a02655c4a70d140f2514c134
MaySparkling <Apriantosmk3@gmail.com>
2026-07-17 20:31:54 +0700

    .

--- app/Models/dosen.php
+++ app/Models/dosen.php
@@ -21,4 +21,23 @@ class Dosen extends Model
         'tanggal_lahir',
         'alamat'
     ];
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function jurusan()
+    {
+        return $this->belongsTo(Jurusan::class, 'jurusan_id');
+    }
+
+    public function kelas()
+    {
+        return $this->hasMany(Kelas::class, 'kode_dosen', 'id');
+    }
+
+    public function matakuliah()
+    {
+        return $this->hasMany(Matakuliah::class, 'dosen_id', 'id');
+    }
 }
